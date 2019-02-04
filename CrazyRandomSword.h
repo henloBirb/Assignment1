0cc903014e85c1a843e366763fceb916a76bb4d3
henloBirb <41307622+henloBirb@users.noreply.github.com>
2019-02-03 19:55:00 -0500

    Create CrazyRandomSword.h
    Implemented header for CrazyRandomSwordClass.

---
+++ CrazyRandomSword.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Hayden Rogers <hpr11@my.fsu.edu>
+ *
+ * Created on Feb 3, 2019
+ */
+
+#include <string>
+#include <ctime>
+#include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+/**
+ * Defines the behavior of a simple hammer (hitpoint = rand between 7-100, ignores rand # 
+ * of armor points ranging between 2 and 1/3 of the armor points)
+ */
+class CrazyRandomSword : public Weapon {
+public:
+	srand(time(0));
+
+    CrazyRandomSword() : Weapon("Crazy random sword", 7 + ( rand() % 93 ) ) { // inspired by examples
+    }
+
+    virtual ~CrazyRandomSword() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* CRAZYRANDOMSWORD_H */
+ 
+ 
